1bbc45fe36f065de5fd2f934ffa12d68f26ecb20
Find Biao <997899594@qq.com>
2026-02-22 22:42:22 +0800

    refactor: move home components to components/shared/home

--- web/ui/home/index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Home Feature - Barrel Export
- */
-
-export { HeroInput } from "./components/HeroInput";
-export { MarkdownRenderer } from "./components/MarkdownRenderer";
-export { RecentCard } from "./components/RecentCard";
-export { RecentSection } from "./components/RecentSection";
