6caf3d6ecb3f1ba7799a991f3eef0a95b57f7cef
Find Biao <997899594@qq.com>
2026-02-22 21:50:14 +0800

    feat: migrate ui package to web/lib/ui

---
+++ apps/web/lib/ui/index.ts
@@ -0,0 +1,15 @@
+export { Button, type ButtonProps, buttonVariants } from "./Button";
+export {
+  ButtonGroup,
+  ButtonGroupSeparator,
+  ButtonGroupText,
+  buttonGroupVariants,
+} from "./ButtonGroup";
+export { Separator } from "./Separator";
+export {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./Tooltip";
+export { cn } from "./utils";
