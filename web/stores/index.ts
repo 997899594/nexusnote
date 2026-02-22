3e5d746a8eb0b5a1908027f4f95ae960c614de5d
Find Biao <997899594@qq.com>
2026-02-22 22:41:41 +0800

    refactor: remove deprecated stores/ directory

--- web/stores/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Stores - Legacy Re-exports
- * @deprecated Use @/ui/auth, @/ui/ai, @/ui/editor
- */
-
-export { useAuthStore } from "@/ui/auth";
-export { type Document, useEditorStore } from "@/ui/editor";
